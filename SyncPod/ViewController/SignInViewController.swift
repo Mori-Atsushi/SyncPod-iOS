806de46d8c4ddca746cdbff11c1f27dfed5eb0c8
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-12 14:43:54 +0900

    [change] フォルダ構造の変更

---
+++ SyncPod/ViewController/SignInViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SignInViewController.swift
+//  SyncPod
+//
+//  Created by 森篤史 on 2018/01/12.
+//  Copyright © 2018年 Cyder. All rights reserved.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController, UINavigationBarDelegate {
+}
