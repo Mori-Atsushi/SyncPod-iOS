031218fd2ca6e0cc292ea02e276368afd5296d5b
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-12 18:57:58 +0900

    [add] 送信ボタン作成

--- SyncPod/ViewController/SignInViewController.swift
+++ SyncPod/ViewController/SignInViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
+extension UITextField {
+    func addBorderBottom(height: CGFloat, color: UIColor) {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: self.frame.height - height, width: self.frame.width, height: height)
+        border.backgroundColor = color.cgColor
+        self.layer.addSublayer(border)
+    }
+}
+
 class SigninViewController: UIViewController, UINavigationBarDelegate {
     @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var mailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationBar.delegate = self
+        
+        mailField.addBorderBottom(height: 1.0, color: UIColor.lightGray)
+        passwordField.addBorderBottom(height: 1.0, color: UIColor.lightGray)
+        submitButton.layer.cornerRadius = 5
     }
 
     func position(for bar: UIBarPositioning) -> UIBarPosition {
