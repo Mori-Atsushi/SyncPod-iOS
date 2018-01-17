e20e8e37c13e489314a73b42e4bf692c2660257a
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-17 13:57:56 +0900

    [add] ログイン成功時の画面遷移を作成

--- SyncPod/ViewController/SignInViewController.swift
+++ SyncPod/ViewController/SignInViewController.swift
@@ -41,6 +41,9 @@ class SignInViewController: UIViewController, UINavigationBarDelegate, HttpReque
 
     func onSuccess(data: JSON) {
         CurrentUser.userToken = data["user"]["access_token"].string
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "TopView") as UIViewController
+        present(viewController, animated: true)
     }
 
     func onFailure(error: Error) {
