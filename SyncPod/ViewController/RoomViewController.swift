acb5bbbefd9f824c7cf66dc8a5f0d73da0f63713
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-19 23:46:43 +0900

    [clear] フォーマッタをかけた

--- SyncPod/ViewController/RoomViewController.swift
+++ SyncPod/ViewController/RoomViewController.swift
@@ -15,40 +15,40 @@ class RoomViewController: UIViewController {
     override func viewDidLoad() {
         setupWebSocket()
     }
-    
+
     private func setupWebSocket() {
         let client = ActionCableClient(url: URL(string: "ws://59.106.220.89:3000/cable/?token=\(CurrentUser.userToken!)")!)
-        
+
         client.willConnect = {
             print("Will Connect")
         }
-        
+
         client.onConnected = {
             print("Connected")
-            let room_identifier = ["room_key" : self.roomKey]
+            let room_identifier = ["room_key": self.roomKey]
             let roomChannel = client.create("RoomChannel", identifier: room_identifier)
             roomChannel.onSubscribed = {
                 print("Subscribed!")
             }
-            
+
             roomChannel.onUnsubscribed = {
                 print("Unsubscribed")
             }
-            
+
             roomChannel.onRejected = {
                 print("Rejected")
             }
         }
-        
-        client.onDisconnected = {(error: ConnectionError?) in
+
+        client.onDisconnected = { (error: ConnectionError?) in
             print("Disconected with error: \(String(describing: error))")
         }
-        
+
         client.willReconnect = {
             print("Reconnecting")
             return true
         }
-        
+
         client.connect()
     }
 }
