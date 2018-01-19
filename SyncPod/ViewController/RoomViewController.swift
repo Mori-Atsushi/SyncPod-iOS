be4374a9a078d9ea1e98da78aa1f36948037d9a6
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-19 10:29:09 +0900

    [add] ActionCableとの接続

--- SyncPod/ViewController/RoomViewController.swift
+++ SyncPod/ViewController/RoomViewController.swift
@@ -7,11 +7,49 @@
 //
 
 import UIKit
+import ActionCableClient
 
 class RoomViewController: UIViewController {
     var roomKey: String = ""
 
     override func viewDidLoad() {
-        print(roomKey)
+        setupWebSocket()
+    }
+    
+    private func setupWebSocket() {
+        let client = ActionCableClient(url: URL(string: "ws://59.106.220.89:3000/cable/?token=\(CurrentUser.userToken!)")!)
+        
+        client.willConnect = {
+            print("Will Connect")
+        }
+        
+        client.onConnected = {
+            print("Connected")
+        }
+        
+        client.onDisconnected = {(error: ConnectionError?) in
+            print("Disconected with error: \(String(describing: error))")
+        }
+        
+        client.willReconnect = {
+            print("Reconnecting")
+            return true
+        }
+        
+        let room_identifier = ["room_key" : roomKey]
+        let roomChannel = client.create("RoomChannel", identifier: room_identifier)
+        roomChannel.onSubscribed = {
+            print("Subscribed!")
+        }
+
+        roomChannel.onUnsubscribed = {
+            print("Unsubscribed")
+        }
+        
+        roomChannel.onRejected = {
+            print("Rejected")
+        }
+        
+        client.connect()
     }
 }
