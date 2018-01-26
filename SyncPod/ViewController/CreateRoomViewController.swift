cede4ad3fb57c6bc528187a776275709592eb9da
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-27 00:04:21 +0900

    [add] Roomを作成

--- SyncPod/ViewController/CreateRoomViewController.swift
+++ SyncPod/ViewController/CreateRoomViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class CreateRoomViewController: UIViewController {
+class CreateRoomViewController: UIViewController, HttpRequestDelegate {
+    
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var descriptionField: UITextView!
     @IBOutlet weak var submitButton: UIButton!
@@ -28,5 +29,42 @@ class CreateRoomViewController: UIViewController {
         nameField.addBorderBottom(height: DeviceConst.textFieldBorderHeight, color: UIColor.lightGray)
         textViewBorder?.removeFromSuperlayer()
         textViewBorder = descriptionField.addBorderBottom(height: DeviceConst.textFieldBorderHeight, color: UIColor.lightGray)
+    }
+    
+    @IBAction func sendCreateRoom(_ sender: UIButton) {
+        let name = nameField.text!
+        let description = descriptionField.text!
+        
+        if validate(name: name, description: description) {
+            sendCreateRoomHttp(name: name, description: description)
+        }
+    }
+    
+    
+    func onSuccess(data: JSON) {
+        print(data)
+    }
+    
+    func onFailure(error: Error) {
+        ErrorAlart(viewController: self, title: "ルーム作成失敗", message: "エラーが発生しました。").show()
+    }
+    
+    private func validate(name: String, description: String) -> Bool {
+        if (name == "" || description == "") {
+            ErrorAlart(viewController: self, title: "アカウント登録失敗", message: "全てのフォームを入力して下さい。").show()
+            return false;
+        }
+        return true
+    }
+    
+    private func sendCreateRoomHttp(name: String, description: String) {
+        let Http = HttpRequestHelper(delegate: self)
+        let data: Parameters = [
+            "room": [
+                "name": name,
+                "description": description
+            ]
+        ]
+        Http.post(data: data, endPoint: "rooms")
     }
 }
