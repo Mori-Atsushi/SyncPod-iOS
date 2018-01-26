3f8e490213c157acd339b98859c61cb85dad6245
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-26 19:57:36 +0900

    [add] 画像を表示

--- SyncPod/CustomView/JoinedRoomsTableViewCell.swift
+++ SyncPod/CustomView/JoinedRoomsTableViewCell.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SwiftyJSON
+import Alamofire
+import AlamofireImage
 
 class JoinedRoomsTabledViewCell: UITableViewCell {
     @IBOutlet weak var name: UILabel!
@@ -28,10 +30,21 @@ class JoinedRoomsTabledViewCell: UITableViewCell {
         self.name.text = room["name"].string!
         self.roomDescription.text = room["description"].string!
         self.onlineMemberNum.text = "オンライン: " + room["online_users"].count.description + "人"
+        var urlString: String?
         if(room["now_playing_video"]["title"].exists()) {
             self.recentVideo.text = "再生中: " + room["now_playing_video"]["title"].string!
+            urlString = room["now_playing_video"]["thumbnail_url"].string
         } else if(room["last_played_video"]["title"].exists()) {
             self.recentVideo.text = "再生終了: " + room["last_played_video"]["title"].string!
+            urlString = room["last_played_video"]["thumbnail_url"].string
+        }
+
+        if(urlString != nil) {
+            Alamofire.request(urlString!).responseImage { response in
+                if let image = response.result.value {
+                    self.thumbnailImage.image = image
+                }
+            }
         }
     }
 }
