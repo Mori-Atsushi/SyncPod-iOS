53f1fe1a7d736b0173cb3cd4fe7b183a3458c273
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-01-29 18:35:37 +0900

    [add] dataを保持するファイルを追加

---
+++ SyncPod/data/PlayList.swift
@@ -0,0 +1,14 @@
+//
+//  PlayList.swift
+//  SyncPod
+//
+//  Created by 森篤史 on 2018/01/29.
+//  Copyright © 2018年 Cyder. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class PlayList {
+    private var list: [Video] = []
+}
---
+++ SyncPod/data/Video.swift
@@ -0,0 +1,16 @@
+//
+//  Video.swift
+//  SyncPod
+//
+//  Created by 森篤史 on 2018/01/29.
+//  Copyright © 2018年 Cyder. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Video {
+    init(video: JSON) {
+        
+    }
+}
