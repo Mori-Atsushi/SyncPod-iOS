24e8de1dc05177bc75719231a57b844bc81e834f
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-02-05 19:32:19 +0900

    [clear] メソッド化

--- SyncPod/AppDelegate.swift
+++ SyncPod/AppDelegate.swift
@@ -16,11 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        if(CurrentUser.userToken == nil) {
-            let storyboard: UIStoryboard = UIStoryboard(name: "FirstStart", bundle: nil)
-            let viewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "FirstStartTopView") as UIViewController
-            window?.rootViewController = viewController
-        }
+        self.selectStartPage()
         
         return true
     }
@@ -28,21 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         // Override point for customization after application launch.
 
-        self.roomKey = nil
-
-        // ユーザがいない場合、初回起動画面に遷移
-        if(CurrentUser.userToken == nil) {
-            let storyboard: UIStoryboard = UIStoryboard(name: "FirstStart", bundle: nil)
-            let viewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "FirstStartTopView") as UIViewController
-            window?.rootViewController = viewController
-        } else if(url.path == "/room") {
-            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let navigationController: UIViewController = storyboard.instantiateViewController(withIdentifier: "NavigationController") as UIViewController
-            window?.rootViewController = navigationController
-            if let roomKey = url.fragments["room_key"] {
-                self.roomKey = roomKey
-            }
-        }
+        self.selectStartPage(url: url)
         
         return true
     }
@@ -68,7 +50,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    private func selectStartPage(url: URL? = nil) {
+        self.roomKey = nil
+        
+        if(CurrentUser.userToken == nil) {
+            let storyboard: UIStoryboard = UIStoryboard(name: "FirstStart", bundle: nil)
+            let viewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "FirstStartTopView") as UIViewController
+            window?.rootViewController = viewController
+        } else if(url?.path == "/room") {
+            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let navigationController: UIViewController = storyboard.instantiateViewController(withIdentifier: "NavigationController") as UIViewController
+            window?.rootViewController = navigationController
+            if let roomKey = url!.fragments["room_key"] {
+                self.roomKey = roomKey
+            }
+        }
+    }
 }
 
