c76b937d4dbcca36b1c8ed4d404400b386d70947
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-03-22 17:39:29 +0900

    [add] 画面の遷移を実装

---
+++ SyncPod/ViewController/UserReportViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserReportViewController.swift
+//  SyncPod
+//
+//  Created by 森篤史 on 2018/03/22.
+//  Copyright © 2018年 Cyder. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class  UserReportViewController: UIViewController {
+    @IBAction func back(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
