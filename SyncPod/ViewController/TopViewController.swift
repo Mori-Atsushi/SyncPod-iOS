f326f46278553d64a2553e167ada370b9dc527e4
Mori Atsushi <mori@cyder.tech>
2018-05-12 16:07:57 +0900

    [add] 人気ルームを表示できるようにした

--- SyncPod/ViewController/TopViewController.swift
+++ SyncPod/ViewController/TopViewController.swift
@@ -12,10 +12,12 @@ import SwiftyJSON
 class TopViewController: UIViewController, HttpRequestDelegate, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var JoinRoomPanel: UIView!
     @IBOutlet weak var CreateRoomPanel: UIView!
-    @IBOutlet weak var TableView: UITableView!
+    @IBOutlet weak var PopularRoomTableView: UITableView!
+    @IBOutlet weak var JoinedRoomTableView: UITableView!
     @IBOutlet weak var ScrillView: UIScrollView!
 
     var joinedRooms: JSON = []
+    var popularRooms: JSON = []
     private let refreshControl = UIRefreshControl()
 
     override func viewDidLoad() {
@@ -27,36 +29,38 @@ class TopViewController: UIViewController, HttpRequestDelegate, UITableViewDataS
         //タッチ制御
         let joinRoomTap = UITapGestureRecognizer(target: self, action: #selector(TopViewController.showJoinRoomAlert(_:)))
         self.JoinRoomPanel.addGestureRecognizer(joinRoomTap)
-        
+
         let createRoomTap = UITapGestureRecognizer(target: self, action: #selector(TopViewController.createRoom(_:)))
         self.CreateRoomPanel.addGestureRecognizer(createRoomTap)
 
-        self.TableView.translatesAutoresizingMaskIntoConstraints = true
-        
+        self.PopularRoomTableView.translatesAutoresizingMaskIntoConstraints = true
+        self.JoinedRoomTableView.translatesAutoresizingMaskIntoConstraints = true
+
         //引っ張って更新
         self.ScrillView.refreshControl = refreshControl
         refreshControl.addTarget(self, action: #selector(TopViewController.refresh(sender:)), for: .valueChanged)
-        
+
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         if let roomKey = appDelegate.roomKey {
             self.joinRoom(roomKey: roomKey)
             appDelegate.roomKey = nil
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         //最近入室したルームの取得
         let Http = HttpRequestHelper(delegate: self)
         Http.get(data: nil, endPoint: "joined_rooms")
+        Http.get(data: nil, endPoint: "rooms/popular")
     }
-    
+
     @objc func refresh(sender: UIRefreshControl) {
         //最近入室したルームの取得
         let Http = HttpRequestHelper(delegate: self)
         Http.get(data: nil, endPoint: "joined_rooms")
-        self.TableView.translatesAutoresizingMaskIntoConstraints = true
+        Http.get(data: nil, endPoint: "rooms/popular")
     }
 
     @objc func showJoinRoomAlert(_ sender: UITapGestureRecognizer) {
@@ -75,11 +79,11 @@ class TopViewController: UIViewController, HttpRequestDelegate, UITableViewDataS
         })
         present(alert, animated: true)
     }
-    
+
     func joinRoom(roomKey: String) {
         self.performSegue(withIdentifier: "JoinRoomSegue", sender: roomKey)
     }
-    
+
     @objc func createRoom(_ sender: UITapGestureRecognizer) {
         self.performSegue(withIdentifier: "CreateRoomSegue", sender: nil)
     }
@@ -92,15 +96,28 @@ class TopViewController: UIViewController, HttpRequestDelegate, UITableViewDataS
     }
 
     func onSuccess(data: JSON) {
-        print(data)
+        print(data["rooms"])
         refreshControl.endRefreshing()
-        joinedRooms = data["joined_rooms"]
-        self.TableView.reloadData()
-        self.TableView.layoutIfNeeded()
-        self.TableView.frame = CGRect(x: TableView.frame.origin.x,
-            y: TableView.frame.origin.y,
-            width: TableView.superview!.frame.width,
-            height: TableView.contentSize.height)
+        if data["joined_rooms"] != JSON.null {
+            joinedRooms = data["joined_rooms"]
+        }
+        if data["rooms"] != JSON.null {
+            popularRooms = data["rooms"]
+        }
+
+        self.PopularRoomTableView.reloadData()
+        self.PopularRoomTableView.layoutIfNeeded()
+        self.PopularRoomTableView.frame = CGRect(x: PopularRoomTableView.frame.origin.x,
+            y: PopularRoomTableView.frame.origin.y,
+            width: PopularRoomTableView.superview!.frame.width,
+            height: PopularRoomTableView.contentSize.height)
+
+        self.JoinedRoomTableView.reloadData()
+        self.JoinedRoomTableView.layoutIfNeeded()
+        self.JoinedRoomTableView.frame = CGRect(x: JoinedRoomTableView.frame.origin.x,
+            y: JoinedRoomTableView.frame.origin.y,
+            width: JoinedRoomTableView.superview!.frame.width,
+            height: JoinedRoomTableView.contentSize.height)
     }
 
     func onFailure(error: Error) {
@@ -110,17 +127,38 @@ class TopViewController: UIViewController, HttpRequestDelegate, UITableViewDataS
     //データを返すメソッド（スクロールなどでページを更新する必要が出るたびに呼び出される）
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Room", for: indexPath as IndexPath) as! JoinedRoomsTabledViewCell
-        cell.setCell(room: joinedRooms[indexPath.row])
+        switch tableView {
+        case PopularRoomTableView:
+            cell.setCell(room: popularRooms[indexPath.row])
+        case JoinedRoomTableView:
+            cell.setCell(room: joinedRooms[indexPath.row])
+        default:
+            break
+        }
         return cell
     }
 
     //データの個数を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return joinedRooms.count
+        switch tableView {
+        case PopularRoomTableView:
+            return popularRooms.count
+        case JoinedRoomTableView:
+            return joinedRooms.count
+        default:
+            return 0
+        }
     }
 
     //タッチされた時の挙動
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.joinRoom(roomKey: joinedRooms[indexPath.row]["key"].string!)
+        switch tableView {
+        case PopularRoomTableView:
+            self.joinRoom(roomKey: popularRooms[indexPath.row]["key"].string!)
+        case JoinedRoomTableView:
+            self.joinRoom(roomKey: joinedRooms[indexPath.row]["key"].string!)
+        default:
+            break
+        }
     }
 }
