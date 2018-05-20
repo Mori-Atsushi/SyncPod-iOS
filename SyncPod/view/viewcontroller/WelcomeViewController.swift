590bc32d434887f97d5ee8840c795b3ab0fd3399
Mori Atsushi <mori@cyder.tech>
2018-05-20 17:48:14 +0900

    [add] databindingを設定

---
+++ SyncPod/view/viewcontroller/WelcomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WelcomeViewController.swift
+//  SyncPod
+//
+//  Created by 森篤史 on 2018/05/20.
+//  Copyright © 2018年 Cyder. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class WelcomeViewConstoller: UIViewController {
+    let disposeBag = DisposeBag()
+    let viewModel = WelcomeViewModel()
+
+    @IBOutlet weak var signInButton: UIBarButtonItem!
+    @IBOutlet weak var signUpButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpDataBinding()
+    }
+
+    private func setUpDataBinding() {
+        signInButton.rx.tap
+            .subscribe({ _ in self.viewModel.onSignInClicked() })
+            .disposed(by: disposeBag)
+        signUpButton.rx.tap
+            .subscribe({ _ in self.viewModel.onSignUpClicked() })
+            .disposed(by: disposeBag)
+    }
+}
---
+++ SyncPod/viewmodel/WelcomeViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewModel.swift
+//  SyncPod
+//
+//  Created by 森篤史 on 2018/05/20.
+//  Copyright © 2018年 Cyder. All rights reserved.
+//
+
+import Foundation
+
+struct WelcomeViewModel {
+    func onSignInClicked() {
+        print("signIn")
+    }
+
+    func onSignUpClicked() {
+        print("signUp")
+    }
+}
