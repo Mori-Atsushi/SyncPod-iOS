96bfb4839462530c01fa3aebd3b6923d05a9919d
Mori Atsushi <mori@cyder.tech>
2018-05-20 16:26:07 +0900

    [add] 初期起動画面を選択（仮実装）

--- SyncPod/AppDelegate.swift
+++ SyncPod/AppDelegate.swift
@@ -1,3 +1,4 @@
+
 //
 //  AppDelegate.swift
 //  SyncPod
@@ -13,12 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication,
-                     open customUrl: URL,
-                     options: [UIApplicationOpenURLOptionsKey: Any] = [:]
-        ) -> Bool {
-        // Override point for customization after application launch.
-
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.selectStartPage()
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        self.selectStartPage()
+        
         return true
     }
 
@@ -35,5 +40,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+    }
+    
+    private func selectStartPage() {
+        let storyboard: UIStoryboard = UIStoryboard(name: "Welcome", bundle: nil)
+        let viewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "WelcomeView") as UIViewController
+        window?.rootViewController = viewController
     }
 }
